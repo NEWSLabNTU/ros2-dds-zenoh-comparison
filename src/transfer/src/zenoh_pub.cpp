1dce427f3b2aae3ac5934db4cff1536964f90065
曾翊綺 <is.vickie18@gmail.com>
2022-12-15 18:18:33 +0800

    my finished zenoh_pub.cpp

--- src/transfer/src/zenoh_pub.cpp
+++ src/transfer/src/zenoh_pub.cpp
@@ -3,6 +3,10 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <zenohc/zenoh.h>
+#include <string.h>
+
+#include "rclcpp/serialization.hpp"
+
 
 using PC2 = sensor_msgs::msg::PointCloud2;
 
@@ -11,7 +15,7 @@ public:
     Transfer() : Node("Transfer") {
         sub_topic = this->declare_parameter("sub_topic", "velodyne_points");
         pub_topic = this->declare_parameter("pub_topic", "my_pub_topic");
-        pub = this->create_publisher<PC2>(pub_topic, 10);
+        pub = z_declare_publisher(z_loan(s), z_keyexpr(pub_topic.c_str()), NULL);
         sub = this->create_subscription<PC2>(
             sub_topic,
             10,
@@ -20,8 +24,13 @@ public:
     }
 
 private:
-    void callback(const PC2::UniquePtr msg) const {
-        pub->publish(*msg);
+    
+    void callback(const PC2::UniquePtr msg) {
+        auto pcd = *msg;
+        rclcpp::SerializedMessage serialized_msg = rclcpp::SerializedMessage(0u);
+        serializer.serialize_message(&pcd, &serialized_msg); 
+        z_publisher_put(z_loan(pub), (const uint8_t *)serialized_msg.get_rcl_serialized_message().buffer, 
+                        serialized_msg.get_rcl_serialized_message().buffer_length, NULL);
         RCLCPP_INFO(
             this->get_logger(),
             "Received message, %d x %d",
@@ -29,11 +38,16 @@ private:
             msg->height
         );
     }
+
     rclcpp::Subscription<PC2>::SharedPtr sub;
-    rclcpp::Publisher<PC2>::SharedPtr pub;
     std::string sub_topic, pub_topic;
     z_owned_config_t config = z_config_default();
     z_owned_session_t s = z_open(z_move(config));
+    z_owned_publisher_t pub;
+    rclcpp::Serialization<PC2> serializer; 
+    
+    
+    
 };
 
 int main(int argc, char ** argv) {
