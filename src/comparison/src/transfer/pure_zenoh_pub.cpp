d7b9c8056e340d2795ab026a3f284c946e7221a5
曾翊綺 <is.vickie18@gmail.com>
2023-01-06 16:38:21 +0800

    rename function

--- src/comparison/src/transfer/pure_zenoh_pub.cpp
+++ src/comparison/src/transfer/pure_zenoh_pub.cpp
@@ -16,35 +16,33 @@ using namespace std::chrono_literals;
 
 std::shared_ptr<PC2> pc2_msg_;
 
-void parse_yaml() {
+void create_sample_msg() {
     pc2_msg_->header.frame_id = "velodyne";
     pc2_msg_->header.stamp.sec = 0;
     pc2_msg_->header.stamp.nanosec = 0;
     pc2_msg_->height = 1800;
     pc2_msg_->width = 32;
+    
     std::vector<sensor_msgs::msg::PointField> pf(6);
-    sensor_msgs::msg::PointField tpf;
-    tpf.name = 'x'; tpf.offset = 0; tpf.datatype = 7; tpf.count = 1;
-    pf[0] = tpf;
-    tpf.name = 'y'; tpf.offset = 4; tpf.datatype = 7; tpf.count = 1;
-    pf[1] = tpf;
-    tpf.name = 'z'; tpf.offset = 8; tpf.datatype = 7; tpf.count = 1;
-    pf[2] = tpf;
-    tpf.name = 'intensity'; tpf.offset = 12; tpf.datatype = 7; tpf.count = 1;
-    pf[3] = tpf;
-    tpf.name = 'ring'; tpf.offset = 16; tpf.datatype = 4; tpf.count = 1;
-    pf[4] = tpf;
-    tpf.name = 'time'; tpf.offset = 18; tpf.datatype = 7; tpf.count = 1;
-    pf[5] = tpf;
+    // sensor_msgs::msg::PointField tpf;
+    // tpf.name = 'x'; tpf.offset = 0; tpf.datatype = 7; tpf.count = 1; pf[0] = tpf;
+    // tpf.name = 'y'; tpf.offset = 4; tpf.datatype = 7; tpf.count = 1; pf[1] = tpf;
+    // tpf.name = 'z'; tpf.offset = 8; tpf.datatype = 7; tpf.count = 1; pf[2] = tpf;
+    // tpf.name = 'intensity'; tpf.offset = 12; tpf.datatype = 7; tpf.count = 1; pf[3] = tpf;
+    // tpf.name = 'ring'; tpf.offset = 16; tpf.datatype = 4; tpf.count = 1; pf[4] = tpf;
+    tpf.name = 'time'; tpf.offset = 18; tpf.datatype = 7; tpf.count = 1; pf[5] = tpf;
     pc2_msg_->fields = pf;
     pc2_msg_->is_bigendian = false;
     pc2_msg_->point_step = 22;
     pc2_msg_->row_step = 704;
-    int size = 704 * 1800; // row_step * height
+
+    int size = 704 * 1800;
     std::vector<uint8_t> data_(size);
     pc2_msg_->data = data_;
+
     return;
 }
+
 class Transfer : public rclcpp::Node {
 public:
     Transfer() : Node(NODE_NAME) {
@@ -99,18 +97,18 @@ public:
     }
 
 private:
+    rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Subscription<PC2>::SharedPtr sub;
     z_owned_session_t session;
     z_owned_publisher_t pub;
-    rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Serialization<PC2> serializer;
     uint8_t message_header_length = 8u;
 };
 
 int main(int argc, char ** argv) {
+    create_sample_msg();
     rclcpp::init(argc, argv);
     pc2_msg_ = std::make_shared<sensor_msgs::msg::PointCloud2>();
-    parse_yaml();
     rclcpp::spin(std::make_shared<Transfer>());
     rclcpp::shutdown();
     return 0;
